e31c60fe47aa50d835e1aba049dc678c1f91b349
isaoohta <tigerwoods1206@yahoo.co.jp>
2013-06-16 22:14:14 +0900

    add appdelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testgit
+//
+//  Created by オオタ イサオ on 2013/06/16.
+//  Copyright (c) 2013年 オオタ イサオ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
